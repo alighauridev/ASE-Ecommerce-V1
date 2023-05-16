87f1bb687231a8195f0ecebc4a1b0ef49fec2587
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-05-16 16:23:49 +0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,17 @@ import Register from './components/User/Register';
 import ProtectedRoute from "./routing/ProtectedRoute";
 import "./App.css";
 import Category from "./components/Category/Category";
+import VendorRegister from "./components/User/VendorRegister";
+import Dashboard from "./components/Admin/Dashboard";
+import MainData from "./components/Admin/MainData.jsx";
+import OrderTable from "./components/Admin/OrderTable.jsx";
+import UpdateOrder from "./components/Admin/UpdateOrder.jsx";
+import ProductTable from "./components/Admin/ProductTable.jsx";
+import NewProduct from "./components/Admin/NewProduct.jsx";
+import UpdateProduct from "./components/Admin/UpdateProduct.jsx";
+import UserTable from "./components/Admin/UserTable.jsx";
+import UpdateUser from "./components/Admin/UpdateUser.jsx";
+import ReviewsTable from "./components/Admin/ReviewsTable.jsx";
 
 function App() {
   return (
@@ -20,6 +31,82 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/vendor/register" element={<VendorRegister />} />
           <Route path="/" element={<HomeScreen />} />
+          <Route path="/admin/dashboard" element={
+
+            <Dashboard activeTab={0}>
+              <MainData />
+            </Dashboard>
+
+          } ></Route>
+
+          <Route path="/admin/orders" element={
+
+            <Dashboard activeTab={1}>
+              <OrderTable />
+            </Dashboard>
+
+          } ></Route>
+
+          <Route path="/admin/order/:id" element={
+
+            <Dashboard activeTab={1}>
+              <UpdateOrder />
+            </Dashboard>
+
+          } ></Route>
+
+          <Route path="/admin/products" element={
+
+            <Dashboard activeTab={2}>
+              <ProductTable />
+            </Dashboard>
+
+
+          } ></Route>
+
+          <Route path="/admin/new_product" element={
+
+            <Dashboard activeTab={3}>
+              <NewProduct />
+            </Dashboard>
+
+          } ></Route>
+
+          <Route path="/admin/product/:id" element={
+
+
+            <Dashboard activeTab={2}>
+              <UpdateProduct />
+            </Dashboard>
+
+
+          } ></Route>
+
+          <Route path="/admin/users" element={
+
+
+            <Dashboard activeTab={4}>
+              <UserTable />
+            </Dashboard>
+
+
+          } ></Route>
+
+          <Route path="/admin/user/:id" element={
+
+            <Dashboard activeTab={4}>
+              <UpdateUser />
+            </Dashboard>
+
+          } ></Route>
+
+          <Route path="/admin/reviews" element={
+
+            <Dashboard activeTab={5}>
+              <ReviewsTable />
+            </Dashboard>
+
+          } ></Route>
 
           <Route path="/category" element={<Category />} />
           <Route element={<ProtectedRoute />}>
